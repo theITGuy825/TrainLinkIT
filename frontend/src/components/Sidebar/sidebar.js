614a4994959cecb2b5150008d47258b775ce9731
ahsan-khan-1 <ahsancalgary@gmail.com>
2025-02-26 19:09:44 -0500

    Added some  changes on profile.js and sidebar.js

--- frontend/src/components/Sidebar/sidebar.js
+++ frontend/src/components/Sidebar/sidebar.js
@@ -8,6 +8,7 @@ function Sidebar() {
       <img src='/image073263.png' alt="description" width="300" height="300" className='logo'/>
 
       <nav>
+        <Link to="/home">Home</Link>
         <Link to="/jobs">Job Board</Link>
         <Link to="/trainings">Trainings</Link>
         <Link to="/blog">Blog</Link>
