a0db8842f3b9f2d4fa140f6a8ca84d2d14cf813b
ahsan-khan-1 <ahsancalgary@gmail.com>
2025-03-26 02:47:15 -0400

    Updated Search bar started working on Messanger

--- src/components/messenger/messenger.js
+++ src/components/messenger/messenger.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Chats from "./Chats"; // The Chats component
 import Messages from "./Messages"; // The Messages component
-
+import Sidebar from "../Sidebar/sidebar.js";
 const Messenger = () => {
   return (
     <div className="messenger">
+      <Sidebar />
       <Chats />
       <Messages />
+
     </div>
   );
 };
