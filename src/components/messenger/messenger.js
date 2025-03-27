3ec71ea9d87626de3172b8eb16df416beae54821
ahsan-khan-1 <ahsancalgary@gmail.com>
2025-03-27 06:10:25 -0400

    made a component that shows following users

--- src/components/messenger/messenger.js
+++ src/components/messenger/messenger.js
@@ -2,13 +2,14 @@ import React from "react";
 import Chats from "./Chats"; // The Chats component
 import Messages from "./Messages"; // The Messages component
 import Sidebar from "../Sidebar/sidebar.js";
+import Followers from "./Following.js";
 const Messenger = () => {
   return (
     <div className="messenger">
       <Sidebar />
       <Chats />
       <Messages />
-
+      <Followers />
     </div>
   );
 };
